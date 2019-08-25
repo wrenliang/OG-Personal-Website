093b99523ce6c5bbf99c5524bafbc775cfa410fc
Wren Liang <wren.liang@gmail.com>
2019-08-25 17:29:23 -0600

    Added prototype image carousel
    Added a working image carousel with filler images.
    Todo:
    -put in real pictures
    -add in animation

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -21,6 +21,35 @@ window.onload = function () {
             elem.style.opacity = opacity;
         }
     }
+
+
+    updateSlides(slideIndex);
+}
+
+
+
+var slideIndex = 1;
+
+function incrementSlide(direction) {
+    updateSlides(slideIndex + direction);
+}
+
+function updateSlides(newIndex) {
+    var slides = document.getElementsByClassName("slides");
+
+    if (newIndex > slides.length) {
+        newIndex = 1;
+    } else if (newIndex < 1){
+        newIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex = newIndex;
+    slides[newIndex - 1].style.display = "block";
+    slides[newIndex - 1].className += " active";
 }
 
 

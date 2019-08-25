db6b4ceeca3ce3933e24e31e515e4eb273662c14
Wren Liang <wren.liang@gmail.com>
2019-08-25 16:13:04 -0600

    Initial Commit

---
+++ scripts/homePage.js
@@ -0,0 +1,26 @@
+//Fade in welcome message
+window.onload = function () {
+    var elem = document.getElementById("welcome");
+    var opacity = 0;
+    var fadeRate = 0.01;
+    elem.style.opacity = opacity;
+    elem.style.visibility = "visible";
+    var interval;
+
+    var startDelay = setTimeout(startAnim, 667);
+
+    function startAnim() {
+        interval = setInterval(stepFade, 10);
+    }
+    
+    function stepFade() {
+        if (opacity >= 1) {
+            clearInterval(interval);
+        } else {
+            opacity += fadeRate;
+            elem.style.opacity = opacity;
+        }
+    }
+}
+
+

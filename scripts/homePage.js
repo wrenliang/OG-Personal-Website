e80dc0bbf68588f5a7892cb421a3247b480c76a6
Wren Liang <wren.liang@gmail.com>
2019-10-22 21:18:16 -0700

    Added fullPage.js framework

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -27,24 +27,77 @@ window.onload = function () {
 
 
 
-    //TESTING THE CP CODE
-        // Wrap every letter in a span
-        var textWrapper = document.querySelector('.ml12');
-        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-        anime.timeline({loop: false})
-        .add({
-        targets: '.ml12 .letter',
-        translateX: [40,0],
-        translateZ: 0,
-        opacity: [0,1],
-        easing: "easeOutExpo",
-        duration: 1250,
-        delay: (el, i) => 500 + 75 * i
-        }).add({
-        });
+    //TESTING THE CP CODE
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.ml12');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: false})
+    .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1250,
+    delay: (el, i) => 500 + 75 * i
+    }).add({
+    });
+
+
+
+
+
+    //Card scroll
+    new fullpage('#fullpage', {
+        //options here
+        menu: '#menu',
+
+        autoScrolling:true,
+        scrollHorizontally: true,
+
+        onLeave: function (origin, destination, direction) {
+            var homePageLink = document.getElementById("homepage-link");
+            var projectsLink = document.getElementById("projects-link");
+            var passionLink = document.getElementById("passion-link");
+            var resumeLink = document.getElementById("resume-link");
+            
+
+            if (destination.index == 1) {
+                homePageLink.classList.add("nav-colored");
+                projectsLink.classList.add("nav-colored");
+                passionLink.classList.add("nav-colored");
+                resumeLink.classList.add("nav-colored");
+                
+                homePageLink.classList.remove("nav-transparent");
+                projectsLink.classList.remove("nav-transparent");
+                passionLink.classList.remove("nav-transparent");
+                resumeLink.classList.remove("nav-transparent");
+            } else {
+                homePageLink.classList.remove("nav-colored");
+                projectsLink.classList.remove("nav-colored");
+                passionLink.classList.remove("nav-colored");
+                resumeLink.classList.remove("nav-colored");
+                
+                homePageLink.classList.add("nav-transparent");
+                projectsLink.classList.add("nav-transparent");
+                passionLink.classList.add("nav-transparent");
+                resumeLink.classList.add("nav-transparent");
+            }
+        }
+    });
+
+    //methods
+    fullpage_api.setAllowScrolling(true);
+
 
 }
+
+
+
+
+
 
 
 window.onscroll = function () {
@@ -65,6 +118,7 @@ window.onscroll = function () {
         rightNav.classList.remove("nav-colored");
     }
 
+    alert();
 }
 
 
